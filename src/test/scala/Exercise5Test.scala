5afdd1ff9c085303c10eb2d9071f143540b5a837
Terapat.Prisiri <Terapat.Prisiri@agoda.com>
2017-12-01 17:21:21 +0700

    Added unit test for timer

--- src/test/scala/Exercise5Test.scala
+++ src/test/scala/Exercise5Test.scala
@@ -2,6 +2,15 @@ import org.scalatest.FunSuite
 
 class Exercise5Test extends FunSuite {
   test("Test timer") {
-    Exercise5.time(Exercise4.gcd(7, 14) == 7)
+    val result = Exercise4.gcd(7, 14)
+    val startTime = System.currentTimeMillis()
+
+    assert(
+      Exercise5.time({
+        Thread.sleep(100)
+        Exercise4.gcd(7, 14)
+      }) == result)
+
+    assert(System.currentTimeMillis() - startTime >= 100)
   }
 }
