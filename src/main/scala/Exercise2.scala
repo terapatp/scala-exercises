41a6de5abf546de9e66916ba607bd6d4d225ff13
Terapat.Prisiri <Terapat.Prisiri@agoda.com>
2017-12-01 12:23:14 +0700

    Implemented stack

--- src/main/scala/Exercise2.scala
+++ src/main/scala/Exercise2.scala
@@ -2,13 +2,29 @@ import Exercise1.ArrayQueue
 
 package object Exercise2 {
 
-  trait Stackable[String] {
-    def put(elem: String)
+  trait Stackable[T] {
+    def push(elem: T)
+
+    def pop: Option[T]
   }
 
-  class ReverseString(capacity: Int) extends ArrayQueue[String](capacity) with Stackable[String] {
-    override def put(input: String) = {
-      super.put(input.reverse)
+  class ReverseStringStack(capacity: Int) extends ArrayQueue[String](capacity) with Stackable[String] {
+    override def push(elem: String): Unit = {
+      super.put(elem.reverse)
+    }
+
+    override def pop: Option[String] = {
+      if (isEmpty) return None
+
+      val result = queue(rear)
+      size = size - 1
+
+      if (rear - 1 < 0)
+        rear = capacity - 1
+      else
+        rear = rear - 1
+
+      Option(result)
     }
   }
 
--- src/test/scala/Exercise2Test.scala
+++ src/test/scala/Exercise2Test.scala
@@ -1,26 +1,44 @@
-import Exercise2.ReverseString
+import Exercise2.ReverseStringStack
 import org.scalatest.FlatSpec
 
 class Exercise2Test extends FlatSpec {
 
-  "A ReverseString queue" should "pop values in last-in-first-out order" in {
-    val queue = new ReverseString(6)
-    queue.put("Not")
-    queue.put("Stack")
-    assert(queue.get.getOrElse() == "toN")
-    assert(queue.get.getOrElse() == "kcatS")
+  "A ReverseString Stack" should "pop values in last-in-first-out order" in {
+    val queue = new ReverseStringStack(6)
+    queue.push("Not")
+    queue.push("Stack")
+
+    assert(queue.pop.getOrElse() == "kcatS")
+    assert(queue.pop.getOrElse() == "toN")
+
+
+    queue.push("Not2")
+    queue.push("Stack2")
+
+    queue.push("Not3")
+    queue.push("Stack3")
+
+    queue.push("Not4")
+    queue.push("Stack4")
+
+    assert(queue.pop.getOrElse() == "4kcatS")
+    assert(queue.pop.getOrElse() == "4toN")
+    assert(queue.pop.getOrElse() == "3kcatS")
+    assert(queue.pop.getOrElse() == "3toN")
+    assert(queue.pop.getOrElse() == "2kcatS")
+    assert(queue.pop.getOrElse() == "2toN")
   }
 
   it should "return None if get from an empty queue" in {
-    val emptyQueue = new ReverseString(5)
-    assert(emptyQueue.get === None)
+    val emptyQueue = new ReverseStringStack(5)
+    assert(emptyQueue.pop === None)
   }
 
   it should "throw IllegalArgumentException if queue is full" in {
-    val queue = new ReverseString(1)
-    queue.put("Not")
+    val queue = new ReverseStringStack(1)
+    queue.push("Not")
     assertThrows[IllegalArgumentException] { // Result type: Assertion
-      queue.put("Full")
+      queue.push("Full")
     }
   }
 }
