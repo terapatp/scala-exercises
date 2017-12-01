6be104eb9fa3c0a4e18e9d46a0b5e75d8ea7a613
Aleksandr <kovalas@yandex.ru>
2017-12-01 17:06:32 +0700

    Update Exercise5Test.scala

--- src/test/scala/Exercise5Test.scala
+++ src/test/scala/Exercise5Test.scala
@@ -1,6 +1,7 @@
 import org.scalatest.FunSuite
 
 class Exercise5Test extends FunSuite {
+  //There are no assertions in this test - how can you be sure that time measured properly and block output is not modified?
   test("Test timer") {
     Exercise5.time(Exercise4.gcd(7, 14) == 7)
   }
